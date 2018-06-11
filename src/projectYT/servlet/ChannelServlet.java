7dc3046ab6381a8b80f9f42d92039a31fbe675f9
Milos Pavicic <milospavicic@yahoo.com>
2018-06-11 11:52:03 +0200

    Edit user fix (temp)

--- src/projectYT/servlet/ChannelServlet.java
+++ src/projectYT/servlet/ChannelServlet.java
@@ -130,7 +130,7 @@ public class ChannelServlet extends HttpServlet {
 		case "edit":
 			try {
 
-				String basicProfileUrl = "pictures/noprofileimage.png";
+				String basicProfileUrl = "http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png";
 
 				String firstName = request.getParameter("firstName");
 				String lastName = request.getParameter("lastName");
